e0628d538e552c78a7330e2fd8a5346f8b0b0de4
Serhii-Pashchenko <paschenko09@gmail.com>
2023-04-02 18:06:59 +0300

    Lesson 28

---
+++ script.js
@@ -0,0 +1,11 @@
+function scale() {
+  let photo = document.querySelector(".photo");
+  photo.classList.toggle("photo-scale");
+}
+
+document.addEventListener("keyup", (event) => {
+  if(event.key === "c" || event.key === "C" || event.key === "l" || event.key === "L" || event.key === "s" || event.key === "S" || event.key === "h" || event.key === "H" || event.key === "w" || event.key === "W" || event.key === "e" || event.key === "E") {
+    const btn = document.getElementById(`${event.key}`);
+    btn.scrollIntoView({behavior: 'smooth'});
+  }
+});
