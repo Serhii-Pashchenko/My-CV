e92618399405f1873b300e66a8a8082f334b60df
Serhii-Pashchenko <paschenko09@gmail.com>
2023-04-13 19:28:45 +0300

    Lesson 32

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function scale() {
 }
 
 document.addEventListener("keyup", (event) => {
-  if(event.key === "c" || event.key === "C" || event.key === "l" || event.key === "L" || event.key === "s" || event.key === "S" || event.key === "h" || event.key === "H" || event.key === "w" || event.key === "W" || event.key === "e" || event.key === "E") {
+  if(event.key === "c" || event.key === "C" || event.key === "l" || event.key === "L" || event.key === "s" || event.key === "S" || event.key === "p" || event.key === "P" || event.key === "h" || event.key === "H" || event.key === "w" || event.key === "W" || event.key === "e" || event.key === "E") {
     const btn = document.getElementById(`${event.key}`);
     btn.scrollIntoView({behavior: 'smooth'});
   }
